72393850e522bb83065a1e73bf5e452a57e91bd6
Andrew Brock <andy.brock@square1tech.com>
2017-01-28 21:22:52 -0600

    Working timestamp augmentation

--- augment_timestamps.pl
+++ augment_timestamps.pl
@@ -6,6 +6,7 @@
 # 00:33  Irssi: #shadows-of-fl: Total of 2 nicks (1 ops, 0 halfops, 0 voices, 1 normal)
 # to look more like:
 # --- Log opened Thu Jan 05 19:14:08 2017
+#
 # 2017-01-05 19:14 -0600-!- Rerogo [~arbrock@chryseis.square1tech.com] has joined #shadows-of-fl
 # 2017-01-05 19:14 -0600-!- Irssi: #shadows-of-fl: Total of 2 nicks [0 ops, 0 halfops, 0 voices, 2 normal]
 # 2017-01-05 19:14 -0600-!- Irssi: Join to #shadows-of-fl was synced in 4 secs
@@ -19,6 +20,20 @@
 # energymech/znc logging from irssi logging.
 
 use strict;
+
+my %months = ('Jan' => '01',
+              'Feb' => '02',
+              'Mar' => '03',
+              'Apr' => '04',
+              'May' => '05',
+              'Jun' => '06',
+              'Jul' => '07',
+              'Aug' => '08',
+              'Sep' => '09',
+              'Oct' => '10',
+              'Nov' => '11',
+              'Dec' => '12');
+
 my $filename = shift @ARGV;
 if ($filename eq "") {
   die "Usage: $0 <in-file>\n";
@@ -26,7 +41,32 @@ if ($filename eq "") {
 
 open(my $fh, "<", $filename) or die "Could not open file $filename.\n";
 
+my $month, my $day, my $year;
+while(my $line = <$fh>) {
+  if($line =~ /Log opened (Sun|Mon|Tues|Wed|Thu|Fri|Sat) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (\d\d) \d\d:\d\d:\d\d (\d{4})/) {
+    $month = $months{$2};
+    $day = $3;
+    $year = $4;
+    print $line;
+  } elsif ($line =~ /Day changed (Sun|Mon|Tues|Wed|Thu|Fri|Sat) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) (\d\d) (\d{4})/) {
+    $month = $months{$2};
+    $day = $3;
+    $year = $4;
+    print $line;
+  } elsif ($line =~ /\d{4}-\d\d-\d\d \d\d:\d\d -\d{4}-!-.*/) {
+    # the rest of the file has good timestamps
+    print $line;
+    last;
+  } else {
+    # regular line
+    $line =~ /(\d\d):(\d\d) (.*)/;
+    my $hour = $1;
+    my $minute = $2;
+    my $rest = $3;
+    print "$year-$month-$day $hour:$minute -0000-!- $rest\n";
+  }
+}
+
 while(my $line = <$fh>) {
   print $line;
-  exit 0;
 }
