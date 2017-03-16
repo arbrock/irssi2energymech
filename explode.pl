4572cc89b8eb3c776cd42c4078249382ce7b53df
Andrew Brock <andy.brock@square1tech.com>
2017-03-16 12:33:10 -0500

    Template for conversion to rotated logs

---
+++ explode.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+# Convert a non-rotated irssi log into a directory of rotated energymech logs
+#
+# Have:
+# --- Log opened Wed Mar 15 17:41:27 2017
+# 2017-03-15 17:41 -0500<arbrock> (but actually I think I'm more likely to see azl living with me than with my sister)
+# 2017-03-15 17:41 -0500<ElenaKarras> Yeah
+# 2017-03-15 17:44 -0500<arbrock> "7.8% from ages 18-24" welp, she's definitely a demographic match for the county...
+# 2017-03-15 17:44 -0500<arbrock> err, town
+# --- Log closed Wed Mar 15 17:49:17 2017
+#
+# Goal:
+# [root@chryseis #felines-lair]# cat 2016-12-09.log
+# [10:47:02] <dancer> chocolate mixed with chili pepper
+# [10:47:06] * dancer purrs
+# [15:37:26] *** Joins: Ralas (~BobArctor@76-198-134-111.lightspeed.sntcca.sbcglobal.net)
+# [15:37:26] *** ChanServ sets mode: +v Ralas
+# [17:44:21] *** Quits: Inglorion (~inglorion@c-76-126-113-28.hsd1.ca.comcast.net) (Ping timeout: 248 seconds)
+# [17:51:45] *** Joins: Inglorion (~inglorion@c-76-126-113-28.hsd1.ca.comcast.net)
+# [19:03:31] *** Ralas is now known as Ralas|afk
+
