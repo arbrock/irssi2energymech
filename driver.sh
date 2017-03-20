59478d8d50bbf7468a90776d5a7c15257e56d1cb
Andrew Brock <arbrock@andrew.cmu.edu>
2017-03-20 04:24:37 -0500

    A buggy driver.sh

---
+++ driver.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+export BASEDIR=`readlink -f $(dirname $0)`
+RUN="sh -vc"
+#RUN="echo"
+find ./ -name '*.log' | xargs -0 -d\\n -I FILE $RUN '
+  export fname=`basename FILE` &&
+  cd `dirname FILE` &&
+  mv ${fname} ${fname}.old &&
+  $BASEDIR/augment_timestamps.pl ${fname}.old > ${fname} &&
+  $BASEDIR/explode.pl ${fname} &&
+  mv ${fname}.old $fname'
